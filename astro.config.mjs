930e51e7496250234a06567b8c47c08c869d0769
X3xu <jesusjimenezfp@gmail.com>
2024-09-03 19:07:42 +0200

    Feat: Todos los archivos .webp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
-import vercelServerless from '@astrojs/vercel/serverless';
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercelServerless({
-    includeFiles: ['./src/assets/CVJesúsJiménezDillana.pdf', './src/assets/Portfolio.webp', './src/assets/QuizzGame.webp'],
+  adapter: vercel({
+    includeFiles: ['./src/assets/CVJesúsJiménezDillana.pdf', './src/assets/*.webp'],
   }),
 
   devToolbar: {
